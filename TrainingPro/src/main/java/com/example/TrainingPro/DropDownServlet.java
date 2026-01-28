6e69e0f7b2fb3317d8b09b7cd43237f68a4a322e
Anuka Snehith Reddy <snehithanuka2006@gmail.com>
2026-01-28 10:53:14 +0530

    Updated training files

---
+++ TrainingPro/src/main/java/com/example/TrainingPro/DropDownServlet.java
@@ -0,0 +1,31 @@
+package com.example.TrainingPro;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/DropDownServlet")
+public class DropDownServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public DropDownServlet() {
+        super();
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username=request.getParameter("username");
+		String email=request.getParameter("email");
+		String gendet=request.getParameter("gender");
+		String country=request.getParameter("country");
+		
+		if(username!=null && !username.isEmpty()) {
+			response.sendRedirect("success.html");
+		}
+		else {
+			response.getWriter().println("failed");
+		}
+	}
+
+}
